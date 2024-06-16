7857841e7fa33c174c5534806b074159ddc43e6b
harshal mali <20harshalmali@gmail.com>
2024-06-16 23:34:15 +0530

    home

--- routes/user.js
+++ routes/user.js
@@ -7,27 +7,29 @@ const { saveRedirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/users.js");
 
-
 //User Singup
-router.route("/signup")
-    .get(userController.renderSignupForm)
-    .post(wrapAsync(userController.signup));
-
+router.route("/signup").get(userController.renderSignupForm).post(wrapAsync(userController.signup));
 
 //User Login
-router.route("/login")
-    .get(userController.renderLoginForm)
-    .post(
-        saveRedirectUrl,
-        passport.authenticate("local", {
-            failureRedirect: "/login",
-            failureFlash: true
-        }),
-        userController.login
-    );
+router
+	.route("/login")
+	.get(userController.renderLoginForm)
+	.post(
+		saveRedirectUrl,
+		passport.authenticate("local", {
+			failureRedirect: "/login",
+			failureFlash: true,
+		}),
+		userController.login
+	);
 
 //User logout
 
 router.get("/logout", userController.logout);
+
+router.get("/", (req, res) => {
+	//redirect home page---------
+	res.redirect("/listings");
+});
 
 module.exports = router;
